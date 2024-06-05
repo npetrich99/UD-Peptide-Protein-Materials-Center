b2b10aa36e75345b055d9f6c6d2c108d123c922f
npetrich99 <nolan.petrich@gmail.com>
2024-06-05 11:58:47 -0400

    Add files via upload
    This code must be run once for the implementation of other R code for Circular Dichroism data analysis. This installs the proper R packages for users.

---
+++ Initial R Package Installation.R
@@ -0,0 +1,26 @@
+
+# Initial R Package Installation
+
+# Code written by Nolan Petrich
+
+# The "CD Temperature and Wavelength Scan Data Analysis" R script provides 
+# general instructions for running the data analysis. However, users will 
+# encounter an error when running the code for the first time if they haven't 
+# used R before. R uses libraries of code for different tasks. Because of this, 
+# these packages often need to be installed. This must be done the first time 
+# before running the automated code. Therefore, this file should be run once, 
+# the first time a user downloads R, to prepare the system for using the 
+# different packages needed to run the automated data analysis file.
+
+# REMINDER: This only needs to be run once.
+
+# Installing Required Packages:
+install.packages("readr")
+install.packages("tidyverse")
+install.packages("ggplot2")
+install.packages("scales")
+install.packages("extrafont")
+install.packages("ggstar")
+install.packages("shiny")
+install.packages("shinyjs")
+install.packages("svglite")
